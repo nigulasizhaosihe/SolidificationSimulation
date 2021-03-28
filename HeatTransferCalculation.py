b6343452b03d1914726f47567906f9ad234c6591
nigulasizhaosihe <34417995+nigulasizhaosihe@users.noreply.github.com>
2021-03-28 21:46:07 +0800

    3.0
    修改了形核开始时间判据；使用插值法确定了固液共存态的热物性参数；正确的在合适的状态选择了合适的热物性参数；下一步的目标是：修改形核模型，修改生长模型，修改传热方程边界条件。

--- HeatTransferCalculation.py
+++ HeatTransferCalculation.py
@@ -5,19 +5,40 @@ class Heatcal:
     def __init__(self):
         self.drawdata = []
         self.time = []
-    def heat_simulation(self,s_h,rho,delta_x,lamda,Told,Tnew,rold,rnew,L,fsold,fsnew):
+    def heat_simulation(self,s_hL,s_hS,rhoL,rhoS,delta_x,lamdaL,lamdaS,Told,Tnew,rold,rnew,L,fsold,fsnew):
 
         endflag = 0                                                          #停止标记
-        delta_t = 0.01 * s_h * rho * delta_x * delta_x / (6 * lamda)         #时间步长
         t = 0                                                                #时间统计
-        ns = 0                                                               #形核数
+        flag = 0                                                             #循环次数统计
+        nv = 0                                                               #形核数
         delta_Tm = 0                                                         #最大过冷度
         TN = 0                                                               #开始形核温度
+        begin = 0                                                            #开始形核标记
+        flag1 = 0                                                            #循环次数的代替
 
         while(endflag == 0):
+
             for i in range(1,3):
                 for j in range(1,3):
                     for k in range(1,3):
+                        '''判断所用参数'''
+                        if(fsold[i][j][k] == 0):
+                            lamda = lamdaL
+                            s_h = s_hL
+                            rho = rhoL
+                        if(0 < fsold[i][j][k] < 1):
+                            lamda = lamdaL + (lamdaS - lamdaL) * fsold[i][j][k]
+                            s_h = s_hS + (s_hL - s_hS) * fsold[i][j][k]
+                            rho = rhoL + (rhoS - rhoL) * fsold[i][j][k]
+                        if(fsold[i][j][k] == 1):
+                            lamda = lamdaS
+                            s_h = s_hS
+                            rho = rhoS
+
+                        '''计算时间步长'''
+                        delta_t = 0.01 * s_h * rho * delta_x * delta_x / (6 * lamda)
+
+                        '''传热方程本体'''
                         qx = lamda * delta_x* delta_x * delta_t * (Told[i - 1][j][k] - Told[i][j][k])
                         qy = lamda * delta_x * delta_x * delta_t * (Told[i][j - 1][k] - Told[i][j][k])
                         qz = lamda * delta_x * delta_x * delta_t * (Told[i][j][k - 1] - Told[i][j][k])
@@ -27,12 +48,20 @@ class Heatcal:
                         q = qx + qy + qz - qix - qiy - qiz
                         Tnew[i][j][k] = Told[i][j][k] + q/(s_h * rho * delta_x * delta_x * delta_x)          #求解当前时刻的温度(不考虑潜热)
 
-                        if(Tnew[i][j][k] <= TN and fsold[i][j][k] != 1):
+                        '''计算形核数'''
+                        if (Tnew[1][1][1] <= TN and nv == 0 ):
+                            nv = ((658200 + 3.814 * ((Tnew[1][1][1] - Told[1][1][1]) / delta_t) * (
+                                    (Tnew[1][1][1] - Told[1][1][1]) / delta_t)) ** 1.5) * 0.87
+                            '''记录形核时的循环次数'''
+                            begin = flag
+                            flag1 = flag + 1
+
+                        if(flag1 > begin and fsold[i][j][k] != 1):
                             '''计算生长速度'''
                             rnew[i][j][k] = (0.6462 * (1160.95 - Told[i][j][k]) * (1160.95 - Told[i][j][k]) / 1000000) * delta_t + rold[i][j][k]
 
                             '''计算固相率场'''
-                            fsnew[i][j][k] = 1 - math.exp((-4/3) * math.pi * ns * rnew[i][j][k] * rnew[i][j][k] * rnew[i][j][k])
+                            fsnew[i][j][k] = 1 - math.exp((-4/3) * math.pi * nv * rnew[i][j][k] * rnew[i][j][k] * rnew[i][j][k])
 
                             '''计算潜热修正后的温度'''
                             delta_fs = fsnew[i][j][k] - fsold[i][j][k]
@@ -43,8 +72,13 @@ class Heatcal:
                             fsold[i][j][k] = fsnew[i][j][k]
                             rold[i][j][k] = rnew[i][j][k]
 
+                            flag1 += 1
+
             '''时间统计'''
             t += delta_t
+
+            '''循环次数统计'''
+            flag += 1
 
             '''计算形核温度'''
             if(Tnew[1][1][1] <= 1160.95 and delta_Tm == 0):
@@ -71,5 +105,6 @@ class Heatcal:
                     for k in range(1,3):
                         Told[i][j][k] = Tnew[i][j][k]
 
+            '''传入绘图数据'''
             self.drawdata.append(Told[1][1][1])
             self.time.append(t)
--- main.py
+++ main.py
@@ -11,9 +11,9 @@ class Solidificationsimulation:
         self.draw = Drawcoolingcurve()
 
     def start(self):
-        self.hs.heat_simulation(self.settings.s_h,self.settings.rho,self.settings.delta_x,
-                                self.settings.lamda,self.settings.T1,self.settings.T2,
-                                self.settings.rold,self.settings.rnew,self.settings.L,
+        self.hs.heat_simulation(self.settings.s_hL,self.settings.s_hS,self.settings.rhoL,self.settings.rhoS,
+                                self.settings.delta_x,self.settings.lamdaL,self.settings.lamdaS,self.settings.T1,
+                                self.settings.T2,self.settings.rold,self.settings.rnew,self.settings.L,
                                 self.settings.fsold,self.settings.fsnew)
 
         self.draw.draw(self.hs.time,self.hs.drawdata)
